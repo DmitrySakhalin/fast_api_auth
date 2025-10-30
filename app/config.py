8ad34e85d44d7754b00f8c0fc585cd8e2765ebc9
Dmitry Pack <dmitrypack0@gmail.com>
2025-10-30 12:57:45 +0300

    edit v.2.1

--- app/config.py
+++ app/config.py
@@ -1,5 +1,4 @@
 import os
-import uuid
 
 SECRET_KEY = os.getenv("SECRET_KEY", "your-very-secret-key")
 ALGORITHM = "HS256"
@@ -9,5 +8,4 @@ DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
 
 TOKEN_EXPIRE_HOURS = 48
 
-# Для UUID токенов генерация
 TOKEN_LENGTH = 36
